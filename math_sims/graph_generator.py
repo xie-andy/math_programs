1c1d2700c60ec0ed3b9bac5d48eef1caa379c8ab
sxie4175 <sxie4175@uni.sydney.edu.au>
2025-05-26 13:29:52 +1000

    added number set generator

--- math_sims/graph_generator.py
+++ math_sims/graph_generator.py
@@ -2,21 +2,26 @@ import random
 import sys
 
 num_nodes = int(input("How many nodes would you like? "))
-num_edges = int(input("How many edges would you like? "))
+num_edges = int(input(f"How many edges would you like? Maximum of {int((num_nodes*(num_nodes-1))/2)}: "))
 
 if (num_edges > ((num_nodes*(num_nodes-1))/2)):
     print("Too many edges for a simple graph. Quitting program.")
     sys.exit()
 
-# edge_weights = bool(input("Would you like your graph to have edge weights? Type True or False: "))
-edge_weight_lower = int(input("Enter the lower bound of your edge weights: "))
-edge_weight_upper = int(input("Enter the upper bound of your edge weights: "))
+edge_weight_input = input("Would you like your graph to have edge weights? Type Y or N: ")
+if (edge_weight_input == "Y"):
+    edge_weights = True
+else:
+    edge_weights = False
+if edge_weights:
+    edge_weight_lower = int(input("Enter the lower bound of your edge weights: "))
+    edge_weight_upper = int(input("Enter the upper bound of your edge weights: "))
+    weights = []
 # directivity = bool(input("Would you like your graph to be directed? Type True or False: "))
 # parallel = bool(input("Would you like to allow parallel edges? Type True or False: "))
 # anti_parallel = bool(input("Would you like to allow anti-parallel edges? Type True or False: "))
 
 edges = []
-weights = []
 
 i = 0
 while i < num_edges:
@@ -26,14 +31,16 @@ while i < num_edges:
         node_b = random.randint(1, num_nodes)
     if ((node_a, node_b) not in edges) and ((node_b, node_a) not in edges):
         edges.append((node_a, node_b))
-        weights.append(random.randint(edge_weight_lower, edge_weight_upper))
+        if edge_weights:
+            weights.append(random.randint(edge_weight_lower, edge_weight_upper))
         i += 1
     else:
         continue
 
 i = 0
 while i < len(edges):
-    print(f"{edges[i]}, Weight: {weights[i]}")
-    # print(edges[i])
-    # print(weights[i])
+    if edge_weights:
+        print(f"{edges[i]}, Weight: {weights[i]}")
+    else:
+        print(f"{edges[i]}")
     i += 1
---
+++ math_sims/number_generator.py
@@ -0,0 +1,75 @@
+import random
+
+# given a mean, median, range and n, generate a random distribution of numbers
+
+mean = 15
+median = 12
+low = 0
+high = 30
+n = 20
+
+def num_generator(mean, median, low, high, n) -> list[int]:
+
+    dist = []
+
+    # idea is to generate a set of 3/4 numbers first (odd or even n), with the middle 1/2 being the median, and the other 2 smaller and bigger to achieve the mean
+    # from there, just generate pairs around the median which maintain the mean property
+
+    # first set generation
+
+    dist.append(median)
+    sum = median
+    first_sum = 3*mean - median
+
+    if (n % 2 == 0):
+        dist.append(median)
+        sum += median
+        first_sum += mean
+        first_sum -= median
+    
+    # next, calculate a pair
+
+    pair_found = False
+
+    while (not pair_found):
+        x = random.randint(low, median)
+        y = first_sum - x
+        if (y >= median and y <= high):
+            pair_found = True
+            dist.append(x)
+            dist.append(y)
+            sum += x
+            sum += y
+
+    # next, fill in all the remaining numbers
+
+    second_sum = 2 * mean
+
+    while (len(dist) < n):
+        x = random.randint(low, median)
+        y = second_sum - x
+        if (y >= median and y <= high):
+            pair_found = True
+            dist.append(x)
+            dist.append(y)
+            sum += x
+            sum += y
+    
+    
+
+    dist.sort()
+
+    set_median = dist[round(n/2)]
+    set_mean = sum/n
+
+    print(set_median) 
+    print(set_mean)
+
+    return(dist)
+
+nums = num_generator(mean, median, low, high, n)
+print(nums)
+
+random.shuffle(nums)
+
+print(nums)
